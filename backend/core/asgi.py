f94f8a05f81f597422428d8bc27d73dbf793932d
Ashish Mishra <i.m.ashishhh@gmail.com>
2026-03-09 16:58:56 +0530

    fix: add missing websocket origin validator import and csrf settings

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -9,6 +9,7 @@ django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 from signaling.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
